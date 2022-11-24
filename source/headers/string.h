8312fc01499a29fc9b71931179bad0a428d6974a
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2022-11-24 20:45:00 +0300

    Add files via upload

--- source/headers/string.h
+++ source/headers/string.h
@@ -11,10 +11,6 @@ Everyone is permitted to copy and distribute verbatim copies of this license doc
 #include <stdlib.h>
 #include <stddef.h>
 
-extern "C"
-{
-#endif
-
 	void	*ft_memcpy(void *dest, const void *src, size_t n);
 	int	ft_memcmp(const void *s1, const void *s2, size_t n);
 	void	*ft_memmove(void *dest, const void *src, size_t len);
@@ -24,5 +20,5 @@ extern "C"
 	int	ft_get_size(int x);
 	static void	ft_fill_res(int size, int offset, int n, char *result);
 	char	*ft_itoa(int x);
-}
+
 #endif
